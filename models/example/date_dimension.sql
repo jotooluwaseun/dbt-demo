8a386014bbaba7a61179bc0f39f99dd63e3a8995
Oluwaseun Joto <jotooluwaseun@gmail.com>
2026-04-01 02:26:40 +0000

    date_dimension model

---
+++ models/example/date_dimension.sql
@@ -0,0 +1,26 @@
+WITH CTE AS (
+    select
+
+    to_timestamp(STARTED_AT) as started_at,
+    date(to_timestamp(STARTED_AT)) as date_started_at,
+    hour(to_timestamp(STARTED_AT)) as hour_started_at,
+
+    case
+    when dayname(to_timestamp(STARTED_AT)) in ('Sat', 'Sun')
+    then 'WEEKEND'
+    else 'BUSSINESSDAY'
+    end as day_type,
+
+    case
+    when month(to_timestamp(STARTED_AT)) in (12, 1, 2) then 'WINTER'
+    when month(to_timestamp(STARTED_AT)) in (3, 4, 5) then 'SPRING'
+    when month(to_timestamp(STARTED_AT)) in (6, 7, 8) then 'SUMMER'
+    else 'AUTUMN'
+    end as station_of_year
+
+
+    from {{ source('demo', 'bike') }}
+    where STARTED_AT != 'started_at'
+)
+
+select * from CTE
